75c7502358ee534dd5849ed562f0d6d3093dccea
Mark L. Chang <mark.chang@gmail.com>
2012-06-26 02:53:29 -0400

    force bigint

--- db/schema.rb
+++ db/schema.rb
@@ -17,9 +17,9 @@ ActiveRecord::Schema.define(:version => 20120626042314) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.integer  "phone"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.integer  "phone",      :limit => 8
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
   end
 
   add_index "contacts", ["email"], :name => "index_contacts_on_email", :unique => true
